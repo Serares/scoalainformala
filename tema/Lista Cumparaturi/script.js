c8d4b142fe21c766bed1ed9c60c0e3643e8c59b3
rares <jianu.rares30@gmail.com>
2018-03-25 21:25:08 +0300

    listaProduse

--- tema/Lista Cumparaturi/script.js
+++ tema/Lista Cumparaturi/script.js
@@ -1,99 +1,104 @@
-
-
 var input = document.querySelector('input[type="text"]');
 var buton = document.querySelector('.btn');
 var divProduse = document.querySelector('.produse');
 var asc = document.querySelector('#asc');
 var desc = document.querySelector('#desc');
+var produseFromDiv = divProduse.getElementsByTagName('p');
+
+
+
+// creez o instanta de obiect
+var objProd = {
+		
+		prod: null,
+		marcat: false
+			
+	}
 
 var listaProd = [];
 
+function mark(index){
 
+	if(listaProd[index].marcat == true){
+		return "marcat";
+	} else {
+		return "nemarcat";
+	}
 
-function addToList(){
-	
-	var valoareInp = input.value;
-	
-	listaProd.push(valoareInp);
 	
 }
 
+// functie care deseneaza html
+function desenare(){
 
+	var html ="<p class='titlu'> Produs </p>";
 
-function addProd(){
-	
-console.log(listaProd);
-	var prod = "";
-	
-	
 	for(let i=0;i<listaProd.length;i++){
 		
+		var rand = `
 		
-		var str = `
-
+		<div><span class="${mark([i])}">${listaProd[i].prod}</span> <span onclick="marcheaza(${i})" class="btnMark">Bifeaza</span> </div>
 		
-			<p>${listaProd[i]} <span class="sterge" onclick="stergeElem(this,event)">Sterge</span></p>
-
-	`;
-		
-		prod += str;
-		
+		`
+		html += rand;
 	}
-	//  pot sa selectez toate butoanele cu clasa respectiva si sa iterez prin ele apoi sa iau evenimentul de pe butonul respectiv si sa ii pun display none parintelui 
-	
-	
-	divProduse.innerHTML = prod;
-	
+
+	divProduse.innerHTML = html;
+
 }
 
+// functie care pusheaza obiectele in array
+ function createArr(elem,e){
+	var val = input.value;
 
-	// stergere elemente
+	var prodct = Object.create(objProd);
 
-	function stergeElem(elem,e){
+	prodct.prod = val;
+	prodct.marcat = false;
 
-		// pot sa iterez prin toate elementele span dupa clasa si la respectivul span sa iau innerHTML
-		console.log(elem.parentNode.innerHTML);
-		elem.parentNode.style.textDecoration = "line-through";
-		listaProd
+	listaProd.push(prodct);
 
-	}
+	desenare();
+
+ }
+
+// functia de marcare a produsului care apeleaza functia draw() dupa ce a setat proprietatea pe true;
+ function marcheaza(ind){
+
+	listaProd[ind].marcat = true;
+	console.log(listaProd[ind]);
+	desenare();
+
+ }
 
 
-	// sortare ascendenta ;
-	function sortareAsc(){
-	
-	
+ function sortareAsc(){
+
 	listaProd.sort(function(a,b){
-		// indiferent de litere mici sau mari
-		var nA = a.toUpperCase();
-		var nB = b.toUpperCase();
-
-		if(nA<nA)
-			return -1;
-		if(nA>nB)
-			return 1;
-		
+		var aG = a.prod;
+		var bG = b.prod;
+		if(aG.toUpperCase()<bG.toUpperCase())
+			return -1
+		if(aG.toUpperCase()>bG.toUpperCase())
+		return 1
+		else 
 		return 0;
-		
 	})
-	
-	addProd();	
-	
-}
-    
-    desc.addEventListener('click', ()=>{
+	desenare();
+ }
 
-        
-		
-		sortareAsc();
 
-        listaProd.reverse();
-            
-        addProd();
+ function sortareDesc(){
 
-    })
-    
-    
-    
-
- 
+	listaProd.sort(function(a,b){
+		var aG = a.prod;
+		var bG = b.prod;
+		if(aG.toUpperCase()<bG.toUpperCase())
+			return 1
+		if(aG.toUpperCase()>bG.toUpperCase())
+		return -1
+		else 
+		return 0;
+	})
+	desenare();
+ }
